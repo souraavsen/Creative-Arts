64090c27a7e70db03553c7b13e4a2bb973249b51
souraavsen1 <sourav15-11392@diu.edu.bd>
2021-10-05 01:16:36 +0600

    Not Found page updated.

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
         <div>
             <div className='flex flex-col items-center w-7/12 py-20 mx-auto mt-28 rounded-md bg-gray-200 mb-28'>
                 <h1 className='text-6xl text-green-500 font-bold pb-8 animate-bounce z-0'>Oops!</h1>
-                <h1 className='text-lg text-gray-900'>404! The Page you ar looking for is not found.</h1>
+                <h1 className='text-lg text-gray-900'>404! The Page you are looking for is not found.</h1>
                 <Link to='/' className='bg-green-500 py-2 px-5 mt-6 rounded-full text-white font-semibold'>Go back to Home</Link>
             </div>
         </div>
