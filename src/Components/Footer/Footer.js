a616c95e48e691151387d3d24617ba4c9532ce30
souraavsen1 <sourav15-11392@diu.edu.bd>
2021-10-05 18:07:14 +0600

    footer updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,19 +10,12 @@ const Footer = () => {
         <div className='flex items-center'>
           <img src={footerimg} width='45px' alt='' />
           <h3 className='text-3xl text-white font-semibold pl-2 '>
-            Creative{" "}
-            <span className='text-green-400 font_akronim'>
-              Arts
-            </span>
+            Creative <span className='text-green-400 font_akronim'>Arts</span>
           </h3>
         </div>
 
         <div>
-          <h3 className='text-lg text-green-400 font-thin text-center'>
-            {" "}
-            “We don’t make mistakes, just happy little accidents.” – Bob Ross
-                  </h3>
-                  {/* Social Links */}
+          {/* Social Links */}
           <div className='flex items-center justify-center py-6'>
             <i class='fab fa-facebook text-white text-3xl hover:text-green-400 cursor-pointer'></i>
             <i class='fab fa-twitter text-white text-3xl px-6 hover:text-green-400 cursor-pointer'></i>
@@ -30,6 +23,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      <h3 className='text-lg text-green-400 font-thin text-center'>
+        {" "}
+        “We don’t make mistakes, just happy little accidents.” – Bob Ross
+      </h3>
       <p className='text-white font-thin text-center py-8'>
         creativearts © 2021. All Rights Reserved.
       </p>
